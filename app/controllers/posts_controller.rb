bf07e316878bb05dcaa8d02d1fb1f35e0d5e58f5
Ryan Heupler <Rheupler@gmail.com>
2015-10-17 22:21:53 -0700

    adds functionality to add posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  def index
+   @posts = Post.all
+   @post = Post.new
+  end
+
+ def new
+   @post = Post.new
+ end
+
+ def destroy
+   @post = Post.find(params[:id])
+   @post.destroy
+   redirect_to posts_path
+ end
+
+ def show
+   @post = Post.find(params[:id])
+
+ end
+
+ def create
+   @post = Post.new(post_params)
+   if @post.save
+     flash[:notice] = "Post added!"
+     redirect_to posts_path
+   else
+     render :index
+   end
+ end
+
+ def edit
+   @post = Post.find(params[:id])
+ end
+
+ def update
+   @post = Post.find(params[:id])
+   if @post.update(post_params)
+     redirect_to post_path
+   else
+     render :edit
+   end
+ end
+
+
+ private
+ def post_params
+   params.require(:post).permit(:title, :content)
+ end
+end
